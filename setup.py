f7dc8bd385254d5ff9c6e00b5bde881422116f96
Joris Jansen <joris97jansen@gmail.com>
2023-08-10 15:01:37 +0200

    Include none compiled files to the library

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from Cython.Build import cythonize
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    packages=find_packages(where="src", exclude=["tests"]),
     ext_modules=cythonize("src/third_python_package/harmonic_mean.pyx"),
 )
